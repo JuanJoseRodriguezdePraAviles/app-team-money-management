46e8e943306b008d3f4bddb67b175b4d10fc0de0
KevinLarriegaP <kevinlarriega@gmail.com>
2022-03-10 19:02:23 +0100

    #4.1 #4.2 #4.3 added model categoria and tramite and their relation closes #17, closes #18, closes #19

--- src/main/java/com/sinensia/model/Categoria.java
+++ src/main/java/com/sinensia/model/Categoria.java
@@ -1,5 +1,39 @@
 package com.sinensia.model;
 
-public class Categoria {
+import java.util.List;
 
+public class Categoria {
+	private int categoriaId;
+	private String nombre;
+	private List<Tramite> listaTramites;
+	
+	public int getCategoriaId() {
+		return categoriaId;
+	}
+	public void setCategoriaId(int categoriaId) {
+		this.categoriaId = categoriaId;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public List<Tramite> getListaTramites() {
+		return listaTramites;
+	}
+	public void setListaTramites(List<Tramite> listaTramites) {
+		this.listaTramites = listaTramites;
+	}
+	
+	public Categoria() {
+		
+	}
+	
+	public Categoria(int categoriaId, String nombre, List<Tramite> listaTramites) {
+		this.categoriaId = categoriaId;
+		this.nombre = nombre;
+		this.listaTramites = listaTramites;
+	}
+	
 }
--- src/main/java/com/sinensia/model/Tramite.java
+++ src/main/java/com/sinensia/model/Tramite.java
@@ -1,5 +1,42 @@
 package com.sinensia.model;
 
 public class Tramite {
+	private int tramiteId;
+	private double valor;
+	private int categoriaId;
+	
+	public int getTramiteId() {
+		return tramiteId;
+	}
 
+	public void setTramiteId(int tramiteId) {
+		this.tramiteId = tramiteId;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public int getCategoriaId() {
+		return categoriaId;
+	}
+
+	public void setCategoriaId(int categoriaId) {
+		this.categoriaId = categoriaId;
+	}
+
+	public Tramite() {
+		
+	}
+	
+	public Tramite(int tramiteId, double valor, int categoriaId) {
+		this.tramiteId = tramiteId;
+		this.valor = valor;
+		this.categoriaId = categoriaId;
+	}
+	
 }
