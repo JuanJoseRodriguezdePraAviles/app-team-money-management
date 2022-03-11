1d989c484c522dd84aa837f47caf9931d23581ff
KevinLarriegaP <kevinlarriega@gmail.com>
2022-03-11 13:16:36 +0100

    #4.9, #4.10 added getList and getById closes #27, closes #28

--- src/main/java/com/sinensia/dao/CategoriaDao.java
+++ src/main/java/com/sinensia/dao/CategoriaDao.java
@@ -1,5 +1,110 @@
 package com.sinensia.dao;
 
-public class CategoriaDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sinensia.contracts.IDao;
+import com.sinensia.model.Categoria;
+
+public class CategoriaDao extends BaseDao implements IDao<Categoria>{
+	private Connection connect;
+	
+	@Override
+	public int add(Categoria modelo) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int modify(Categoria modelo) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Categoria> get() throws SQLException {
+		PreparedStatement preparedStatement = null;
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		ResultSet rs = null;
+		
+		try {
+			connect = super.getconnection();
+			preparedStatement = connect.prepareStatement("SELECT * FROM categoria");
+			
+			rs = preparedStatement.executeQuery();
+			
+			while(rs.next()) {
+				Categoria categoria = new Categoria();
+				categoria.setCategoriaId(rs.getInt("categoriaId"));
+				categoria.setNombre(rs.getString("nombre"));
+				categoria.setEsIngreso(rs.getBoolean("esIngreso"));
+				categorias.add(categoria);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(preparedStatement != null) {
+				preparedStatement.close();
+			}
+			if(rs != null) {
+				rs.close();
+			}
+			if(connect != null) {
+				connect.close();
+			}
+			
+		}
+		
+		return categorias;
+	}
+
+	@Override
+	public Categoria getById(int id) throws SQLException {
+		PreparedStatement preparedStatement = null;
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		ResultSet rs = null;
+		Categoria categoria = null;
+		
+		try {
+			connect = super.getconnection();
+			preparedStatement = connect.prepareStatement("SELECT * FROM categoria");
+			
+			rs = preparedStatement.executeQuery();
+			
+			categoria = new Categoria();
+			categoria.setCategoriaId(rs.getInt("categoriaId"));
+			categoria.setNombre(rs.getString("nombre"));
+			categoria.setEsIngreso(rs.getBoolean("esIngreso"));
+			categorias.add(categoria);
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(preparedStatement != null) {
+				preparedStatement.close();
+			}
+			if(rs != null) {
+				rs.close();
+			}
+			if(connect != null) {
+				connect.close();
+			}
+			
+		}
+		
+		return categoria;
+	}
+
+	@Override
+	public int remove(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
