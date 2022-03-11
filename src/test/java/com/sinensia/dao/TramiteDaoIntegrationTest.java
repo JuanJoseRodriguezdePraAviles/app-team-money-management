da312f3cee784f7ca6799a1d683b992da8c4e08e
JuanjoUps <juanjo.arribas.p@gmail.com>
2022-03-11 15:03:58 +0100

    feature/TramitesDaoIntegrationTest methrod getById

---
+++ src/test/java/com/sinensia/dao/TramiteDaoIntegrationTest.java
@@ -0,0 +1,69 @@
+package com.sinensia.dao;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.sinensia.contracts.IDao;
+import com.sinensia.model.Tramite;
+
+public class TramiteDaoIntegrationTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+	private static int idtramite = 1;
+	private static int categoriaId = 1;
+	@Test
+	public void testAdd() throws SQLException {
+		
+		Tramite tramite = new Tramite();
+		
+		IDao<Tramite> listaITramiteDao = new TramiteDao();
+		
+		tramite.setTramiteId(idtramite);
+		tramite.setValor(22.2);
+		tramite.setCategoriaId(categoriaId);
+		tramite.setConcepto("nomina");
+		tramite.setFecha(LocalDate.parse("2020-04-05"));
+		
+		
+		idtramite =  listaITramiteDao.add(tramite);
+		categoriaId = listaITramiteDao.add(tramite);
+		assertTrue(idtramite>0);
+		assertTrue(categoriaId>0);
+		
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetById() {
+		fail("Not yet implemented");
+	}
+
+}
