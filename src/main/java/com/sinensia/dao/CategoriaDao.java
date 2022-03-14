477c5ed79b8b44d8633989f482a7e1b90db9dfed
JuanjoUps <juanjo.arribas.p@gmail.com>
2022-03-14 12:18:21 +0100

    feature/refactor Add from

--- src/main/java/com/sinensia/dao/CategoriaDao.java
+++ src/main/java/com/sinensia/dao/CategoriaDao.java
@@ -7,8 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.ServletContextEvent;
-
 import com.sinensia.contracts.IDao;
 import com.sinensia.model.Categoria;
 
